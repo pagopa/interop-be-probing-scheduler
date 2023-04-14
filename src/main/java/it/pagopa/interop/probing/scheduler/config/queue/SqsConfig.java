3661f118b8b2db88eb7ba46f9166d4eaf024aed4
AlessandroColucci <107028283+AlessandroColucci@users.noreply.github.com>
2023-04-14 11:46:01 +0200

    SQS implementation (#3)

---
+++ src/main/java/it/pagopa/interop/probing/scheduler/config/queue/SqsConfig.java
@@ -0,0 +1,19 @@
+package it.pagopa.interop.probing.scheduler.config.queue;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+
+@Configuration
+public class SqsConfig {
+
+
+  @Bean
+  public AmazonSQSAsync amazonSQSAsync() {
+    return AmazonSQSAsyncClientBuilder.standard()
+        .withCredentials(new DefaultAWSCredentialsProviderChain()).build();
+  }
+
+}
---
+++ src/main/java/it/pagopa/interop/probing/scheduler/dto/PollingActiveEserviceResponse.java
@@ -0,0 +1,20 @@
+package it.pagopa.interop.probing.scheduler.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import it.pagopa.interop.probing.scheduler.util.EserviceTechnology;
+import lombok.Data;
+
+
+@Data
+public class PollingActiveEserviceResponse {
+
+  @JsonProperty("id")
+  private String id;
+
+  @JsonProperty("technology")
+  private EserviceTechnology technology;
+
+  @JsonProperty("basePath")
+  private String[] basePath;
+
+}
