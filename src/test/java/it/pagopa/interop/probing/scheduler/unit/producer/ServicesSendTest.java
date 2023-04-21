d02ed4d5913b916bd1142e96268cff45214dfee0
acolucci <a.colucci@dxc.com>
2023-04-21 13:43:47 +0200

    JUnit tests

--- src/test/java/it/pagopa/interop/probing/scheduler/unit/producer/ServicesSendTest.java
+++ src/test/java/it/pagopa/interop/probing/scheduler/unit/producer/ServicesSendTest.java
@@ -1,13 +1,14 @@
-package it.pagopa.interop.probing.scheduler;
+package it.pagopa.interop.probing.scheduler.unit.producer;
 
 import java.io.IOException;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,7 +17,7 @@ import it.pagopa.interop.probing.scheduler.producer.ServicesSend;
 import it.pagopa.interop.probing.scheduler.util.EserviceTechnology;
 
 @SpringBootTest
-class ServiceSendTest {
+class ServicesSendTest {
 
   @InjectMocks
   ServicesSend servicesSend;
@@ -26,13 +27,15 @@ class ServiceSendTest {
 
   private EserviceContent eServiceDTO;
 
+  private static final String TEST_URL = "http://queue/test-queue";
+
   @Mock
   ObjectMapper mapper;
 
-  @BeforeAll
+  @BeforeEach
   void setup() {
     String[] basePath = {"basePath1", "basePath2"};
-
+    ReflectionTestUtils.setField(servicesSend, "sqsUrl", TEST_URL);
     eServiceDTO = EserviceContent.builder().basePath(basePath).id("1")
         .technology(EserviceTechnology.REST).build();
 
@@ -41,10 +44,9 @@ class ServiceSendTest {
   @Test
   @DisplayName("The sendMessage method of ServicesSend class is tested.")
   void testSendMessage_whenGivenValidEServiceAndUrl_thenSchedulerWriteOnQueue() throws IOException {
-    String url = "http://queue/test-queue";
     Mockito.when(amazonSQS.sendMessage(Mockito.any())).thenReturn(null);
     servicesSend.sendMessage(eServiceDTO);
-    SendMessageRequest sendMessageRequest = new SendMessageRequest().withQueueUrl(url)
+    SendMessageRequest sendMessageRequest = new SendMessageRequest().withQueueUrl(TEST_URL)
         .withMessageBody(mapper.writeValueAsString(eServiceDTO));
     Mockito.verify(amazonSQS).sendMessage(sendMessageRequest);
   }
