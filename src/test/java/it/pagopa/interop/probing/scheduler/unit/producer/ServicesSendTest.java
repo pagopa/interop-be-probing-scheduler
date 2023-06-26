e0417e2904bd7507c5b750598c57fc1507538a1c
angelaantonia.cea <angelaantonia.cea@dxc.com>
2023-06-26 15:24:08 +0200

    Fixed junit test due to x-ray implementation

--- src/test/java/it/pagopa/interop/probing/scheduler/unit/producer/ServicesSendTest.java
+++ src/test/java/it/pagopa/interop/probing/scheduler/unit/producer/ServicesSendTest.java
@@ -1,6 +1,7 @@
 package it.pagopa.interop.probing.scheduler.unit.producer;
 
 import java.io.IOException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.xray.AWSXRay;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.interop.probing.scheduler.dto.impl.EserviceContent;
 import it.pagopa.interop.probing.scheduler.producer.ServicesSend;
@@ -40,6 +43,7 @@ class ServicesSendTest {
 
   @BeforeEach
   void setup() {
+    AWSXRay.beginSegment("Segment-test");
     String[] basePath = {"basePath1", "basePath2"};
     String[] audience = {"audience"};
     ReflectionTestUtils.setField(servicesSend, "sqsUrl", TEST_URL);
@@ -48,14 +52,22 @@ class ServicesSendTest {
 
   }
 
+  @AfterEach
+  void clean() {
+    AWSXRay.endSegment();
+  }
+
   @Test
   @DisplayName("The sendMessage method of ServicesSend class is tested.")
   void testSendMessage_whenGivenValidEServiceAndUrl_thenSchedulerWriteOnQueue() throws IOException {
     Mockito.when(amazonSQS.sendMessage(Mockito.any())).thenReturn(null);
     servicesSend.sendMessage(eServiceDTO);
-    SendMessageRequest sendMessageRequest = new SendMessageRequest().withQueueUrl(TEST_URL)
-        .withMessageBody(mapper.writeValueAsString(eServiceDTO));
-    Mockito.verify(amazonSQS).sendMessage(sendMessageRequest);
+    SendMessageRequest expectedSendMessageRequest = new SendMessageRequest().withQueueUrl(TEST_URL)
+        .withMessageBody(mapper.writeValueAsString(eServiceDTO))
+        .addMessageAttributesEntry("AWSTraceHeader",
+            new MessageAttributeValue().withDataType("String").withStringValue("test"));
+
+    Mockito.verify(amazonSQS).sendMessage(expectedSendMessageRequest);
   }
 
 
